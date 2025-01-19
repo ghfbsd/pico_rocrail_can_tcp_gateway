5ea6e79a7281ed477fe44ed533a7055051427774
ghfbsd <ghfbsd@gmail.com>
2025-01-19 22:59:35 +0000

    Initial commit of relevant code

---
+++ TCP-CAN.py
@@ -0,0 +1,285 @@
+# TCP - CAN bus router
+
+# CPU: Raspberry Pi pico W
+# CAN device: RB-P-CAN-RS485 board by Joy-IT (www.joy-it.net)
+
+# Runs with:
+# MicroPython v1.24.1 on 2024-11-29; Raspberry Pi Pico W with RP2040
+
+# 16 Jan. 2025
+# last revision 19 Jan. 2025
+
+# See the LICENSE file associated with this repository.
+# See https://github.com/ghfbsd
+
+VER = 'AN195'                    # version ID
+
+SSID = "********"
+PASS = "********"
+
+ROCRAIL_PORT = 15731             # Marklin diktat
+ROCSIZE = 13                     # Fixed by protocol definition
+
+QSIZE = 25                       # Size of various I/O queues (overkill)
+
+import uasyncio as asyncio
+import network
+import sys
+from time import sleep
+
+from micropython import alloc_emergency_exception_buf as AEEB
+AEEB(100)                        # boilerplate for IRQ-level exception reporting
+
+from machine import Pin
+pico_led = Pin("LED", Pin.OUT)
+
+class iCAN:                      # interrupt driven CAN message sniffer
+   from canbus import Can, CanError
+   from canbus.internal import CAN_SPEED
+   from machine import Pin, SPI
+
+   prep = SPI(0,                 # configure SPI to use correct pins
+      sck=Pin(18), mosi=Pin(19), miso=Pin(16)
+   )
+
+   def __init__(self):
+      from machine import Pin
+      from canbus import Can, CanError
+      from canbus.internal import CAN_SPEED
+
+      self.flag = asyncio.ThreadSafeFlag()
+      self.pin = Pin(15,Pin.IN,Pin.PULL_UP)
+      self.pin.irq(              # this defines the interrupt handler (lambda)
+         trigger=Pin.IRQ_FALLING,
+         handler=lambda pin: self.flag.set(),
+         hard=True)
+      self.can = Can()
+      # Initialize the CAN interface.  Reference says 250 kbps speed.
+      ret = self.can.begin(bitrate=CAN_SPEED.CAN_250KBPS)
+      if ret != CanError.ERROR_OK:
+         raise RuntimeError('Error initializing CAN bus')
+      print("CAN initialized successfully, waiting for traffic.")
+
+   def intf(self):               # hardware interface level access if needed
+      return self.can
+
+   def run(self, proc):
+      # Method never returns; calls proc upon every interrupt.
+      # Usage:
+      #    proc(msg, err) 
+      # with
+      #    msg - Can message
+      #    err - True if read error; False if OK
+      # no return value
+
+      task = asyncio.create_task(self.intr(proc))
+      Loop.run_until_complete(task)
+
+   async def intr(self, proc):
+      # Handle CAN card interrupt.
+      # Response to any interrupt is implemented via a callback method
+      # rather than as a generator (using e.g. async for x in iCan(): ...)
+      # because the interrupt response appears to be faster.  This is
+      # due to the checkReceive() loop in the handler itself.
+
+      from canbus import CanError
+
+      await asyncio.sleep(0)
+      while True:
+         await self.flag.wait()
+         intr = self.can.getInterrupts()
+         if intr & 0x03 == 0:
+            # ignore interrupts except for reading
+            self.can.clearInterrupts()
+            continue
+         while self.can.checkReceive():
+            # CAN error code and message
+            error, msg = self.can.recv()
+            proc(msg, error != CanError.ERROR_OK)
+            await asyncio.sleep(0)
+         self.can.clearInterrupts()
+
+   def send(self, ID=None, data=None, EFF=False, RTR=False):
+      # Send a CAN message.
+      # Called with:
+      #   ID - (integer) CAN id
+      #   data - (bytearray) data payload
+      #   EFF - set EFF flag in frame for long IDs
+      #   RTR - set RTR flag in frame
+      # returns
+      #   True if send error, False if sent OK
+
+      from canbus import CanMsg, CanMsgFlag, CanError
+
+      cflg =  CanMsgFlag.EFF if EFF else 0
+      cflg |= CanMsgFlag.RTR if RTR else 0
+      msg = CanMsg(can_id=ID, data=data, flags=cflg)
+      return self.can.send(msg) != CanError.ERROR_OK
+
+   def stop(self):
+      # Turns off CAN interrupt handling, disabling board for reading.
+      self.pin.irq(handler=None)
+
+from threadsafe import ThreadSafeQueue
+
+CANtoTCP = ThreadSafeQueue(QSIZE*[bytearray(ROCSIZE)])
+TCPtoCAN = ThreadSafeQueue(QSIZE*[bytearray(ROCSIZE)])
+debugQUE = ThreadSafeQueue(QSIZE*[bytearray(ROCSIZE)])
+
+TCP_R, TCP_W = None, None
+
+rrhash = 0
+
+async def TCP_SERVER(R,W):
+   # Callback when RocRail client connects to us
+   global TCP_R, TCP_W
+   print('TCP connection made, waiting for traffic.')
+   TCP_R, TCP_W = R, W
+
+async def TCP_READER():
+   # packet layout:
+   #    xx xx xx xx  xx  xx xx xx xx xx xx xx xx  -  13 bytes total = ROCSIZE
+   #    -----------  --  -----------------------
+   #       CAN ID    len  data (left justified)
+   global TCP_R, rrhash
+   while True:
+      if TCP_R is None:
+         await asyncio.sleep_ms(10)
+         continue
+      pkt = await TCP_R.readexactly(ROCSIZE)
+      assert len(pkt) == ROCSIZE
+      rrhash = int.from_bytes(pkt[2:4])
+      await TCPtoCAN.put(pkt)
+      await debugQUE.put(pkt)
+
+async def CAN_READER():
+   global can
+   can = iCAN()
+   can.run(CAN_IN)
+
+def CAN_IN(msg, err, buf=bytearray(ROCSIZE)):
+   if err:
+      print('   >>>CAN read error<<<')
+      return
+   buf[0] = msg.can_id >> 24 & 0xff 
+   buf[1] = msg.can_id >> 16 & 0xff 
+   buf[2] = msg.can_id >>  8 & 0xff 
+   buf[3] = msg.can_id       & 0xff
+   buf[4] = msg.dlc
+   buf[5:14] = 8*b'0'
+   for i in range(msg.dlc): buf[5 + i] = msg.data[i]
+   try:
+      CANtoTCP.put_sync(buf)     # put_sync because no await used
+   except:
+      print('***CANtoTCP q full')
+   try:
+      debugQUE.put_sync(buf)     # pub_sync because no await used
+   except:
+      print('***debug q full')
+
+async def TCP_WRITER():
+   global TCP_W
+   while TCP_W is None:
+      await asyncio.sleep_ms(10)
+   async for pkt in CANtoTCP:
+      assert len(pkt) == ROCSIZE
+      TCP_W.write(pkt)
+      TCP_W.drain()
+
+async def CAN_WRITER():
+   global can
+   async for pkt in TCPtoCAN:
+      assert len(pkt) == ROCSIZE
+      while can.send(
+            ID=int.from_bytes(pkt[0:4]),
+            data=pkt[5:5+int(pkt[4])],
+            EFF=True
+         ):
+         print('   >>>CAN write error<<<')
+         asyncio.sleep_ms(10)
+
+try:
+   from marklin import decode    # Marklin CS2 CAN packet decoder
+   avail = True
+except:
+   def decode(ID, data, detail=None):
+      return ''
+   avail = False                 # don't use it if not available
+   print('No Märklin packet decoding, only logging raw data.')
+
+async def DEBUG_OUT():
+   global rrhash, avail
+   async for buf in debugQUE:
+      assert len(buf) == ROCSIZE
+      data = ' '.join(           # put space between every octet
+         map(''.join, zip(*[iter(buf.hex())]*2))
+      )
+      cid = int.from_bytes(buf[2:4])
+      if cid == rrhash:
+         print('TCP -> CAN %s' % data)
+      else:
+         print('CAN -> TCP %s' % data)
+      if avail:
+         print('   %s' % decode(
+               int.from_bytes(buf[0:4]), buf[5:5+int(buf[4])], detail=True
+            )
+         )
+      if TCPtoCAN.qsize() > 0 and TCPtoCAN.qsize() % 5 == 0:
+         print('TCPtoCAN queue congestion: %d waiting' % TCPtoCAN.qsize())
+      if CANtoTCP.qsize() > 0 and CANtoTCP.qsize() % 5 == 0:
+         print('CANtoTCP queue congestion: %d waiting' % CANtoTCP.qsize())
+
+async def HEARTBEAT():
+   # Slow LED flash heartbeat while running; boot button restarts.
+   global pico_led
+   while True:
+      if rp2.bootsel_button() == 1: sys.exit()
+      pico_led.on()
+      await asyncio.sleep(1)
+      pico_led.off()
+      await asyncio.sleep(1)
+
+from asyncio import Loop
+
+print('TCP <-> CAN packet router (%s)' % VER)
+try:
+   from marklin import decode    # Marklin CS2 CAN packet decoder
+   avail = True
+except:
+   def decode(ID, data, detail=None):
+      return ''
+   avail = False                 # don't use it if not available
+   print('No Märklin packet decoding, only logging raw data.')
+
+#Connect to WLAN
+wlan = network.WLAN(network.STA_IF)
+if not wlan.isconnected():
+    wlan.active(True)
+    mac = wlan.config('mac')
+    host = 'rpp-' + ''.join('{:02x}'.format(b) for b in mac[3:])
+    wlan.config(hostname = host)
+    wlan.connect(SSID, PASS)
+else:
+    host = wlan.config('hostname')
+
+while not wlan.isconnected():
+   # Fast flash while waiting for wifi connection; boot button restarts.
+   if rp2.bootsel_button() == 1: sys.exit()
+
+   pico_led.on()
+   sleep(0.25)
+   pico_led.off()
+   sleep(0.25)
+
+pico_led.on()
+ip = wlan.ifconfig()[0]
+print('Connected on {} as {}'.format(ip,host))
+
+canr = asyncio.create_task(CAN_READER())
+tcpr = asyncio.create_task(TCP_READER())
+tcpw = asyncio.create_task(TCP_WRITER())
+canw = asyncio.create_task(CAN_WRITER())
+dbug = asyncio.create_task(DEBUG_OUT())
+beat = asyncio.create_task(HEARTBEAT())
+
+Loop.run_until_complete(asyncio.start_server(TCP_SERVER,ip,ROCRAIL_PORT))
---
+++ marklin.py
@@ -0,0 +1,286 @@
+# Decodes CAN packet according to Marklin protocol
+
+# See the LICENSE file associated with this repository.
+
+# See https://github.com/ghfbsd
+
+def decode(ID,data,detail=False) -> str:
+   hash = ID & 0xffff
+   resp = 'R' if ID >> 16 & 0x01 == 1 else 'C'
+   comm = ID >> 17 & 0xff
+   prio = ID >> 25 & 0x03
+   dlen = len(data)
+   dhex = ' '.join(           # put space between every octet
+       map(''.join, zip(*[iter(data.hex())]*2))
+   )
+   gen = '%s %02x hash %04x prio: %d data: %s' % (
+          resp, comm, hash, prio, dhex if dlen > 0 else ''
+      )
+
+   if not detail:
+      return gen
+
+   if comm == 0x00:          # SYSTEM
+      mess = resp + ' SYSTEM'
+      if dlen < 5:
+         mess += ' (garbled)'
+         return mess
+      sub = int(data[4])
+      if sub == 0x00:        # STOP
+         mess += ' STOP 00/00'
+         if data[0:4] == 4*b'\x00':
+            mess += ' (everybody)'
+         else:
+            mess += ' (%s)' % data[0:4].hex()
+         return mess
+      if sub == 0x01:        # GO
+         mess += ' GO 00/01'
+         if data[0:4] == 4*b'\x00':
+            mess += ' (everybody)'
+         else:
+            mess += ' (%s)' % data[0:4].hex()
+         return mess
+      if sub == 0x02:        # SYSTEM HALT
+         mess += ' HALT 00/02'
+         if data[0:4] == 4*b'\x00':
+            mess += ' (everybody)'
+         else:
+            mess += ' (%s)' % data[0:4].hex()
+         return mess
+      if sub == 0x03:        # LOCO EMERGENCY STOP
+         mess += ' EMERGENCY HALT 00/03 (%s)' % data[0:4].hex()
+         return mess
+      if sub == 0x04:        # LOCO FORGET
+         mess += ' LOCO FORGET 00/04'
+         if data[0:4] == 4*b'\x00':
+            mess += ' (everybody)'
+         else:
+            mess += ' (%s)' % data[0:4].hex()
+         return mess
+      if sub == 0x05:        # LOCO DATA PROTOCOL
+         if dlen != 6:
+            mess += ' LOCO PROTOCOL CHANGE 00/05 (%s): garbled' % (
+               data[0:4].hex(),
+            )
+            return mess
+         mess += ' LOCO PROTOCOL CHANGE 00/05 (%s): %s' % (
+            data[0:4].hex(),data[5].hex()
+         )
+         return mess
+      if sub == 0x06:        # ACCESSORY SWITCH TIME
+         if dlen != 7:
+            mess += ' ACCESSORY SWITCHING TIME 00/06 (%s): garbled' % (
+               data[0:4].hex(),
+            )
+            return mess
+         mess += ' ACCESSORY SWITCH TIME 00/06 (%s): %d ms' % (
+            data[0:4].hex(),int.from_bytes(data[5:7])
+         )
+         return mess
+      if sub == 0x08:        # PROTOCOL AVAILABILITY
+         if dlen != 6:
+            mess += ' PROTOCOL AVAILABILITY 00/08 (%s): garbled' % (
+               data[0:4].hex(),
+            )
+            return mess
+         mess += ' PROTOCOL AVAILABILITY 00/08 (%s):' % data[0:4].hex()
+         if data[5] & 0x01: mess += ' MM2'
+         if data[5] & 0x02: mess += ' MFX'
+         if data[5] & 0x04: mess += ' DCC'
+         return mess
+      if sub == 0x09:        # MFX NEW REGISTRATION COUNTER
+         if dlen != 7:
+            mess += ' MFX NEW REG COUNTER 00/09 (%s): garbled' % (
+               data[0:4].hex(),
+            )
+            return mess
+         mess += ' MFX NEW REG COUNTER 00/09 (%s): %d' % (
+            data[0:4].hex(),int.from_bytes(data[6:8])
+         )
+         return mess
+      if sub == 0x0a:        # OVERLOAD
+         if dlen != 6:
+            mess += ' OVERLOAD! 00/0A (%s): garbled' % (
+               data[0:4].hex(),
+            )
+            return mess
+         mess += ' OVERLOAD! 00/0A (%s): channel %d' % (
+            data[0:4].hex(), int(data[5])
+         )
+         return mess
+      if sub == 0x0b:        # STATUS
+         if dlen == 6 and resp == 'C':
+            mess += ' STATUS 00/0B (%s): channel %d' % (
+               data[0:4].hex(), int(data[5])
+            )
+         if dlen == 8 and resp == 'C':
+            mess += ' STATUS 00/0B (%s): channel %d config %d' % (
+               data[0:4].hex(),
+               int(data[5]),
+               int.from_bytes(data[6:8])
+            )
+            return mess
+         if dlen == 7 and resp == 'R':
+            mess += ' STATUS 00/0B (%s): channel %d is %s' % (
+               data[0:4].hex(), int(data[5]), 1 == int(data[6])
+            )
+            return mess
+         if dlen == 7 and resp == 'R':
+            mess += ' STATUS 00/0B (%s): channel %d is %s' % (
+               data[0:4].hex(), int(data[5]), 1 == int(data[6])
+            )
+            return mess
+         mess += ' STATUS 00/0B (%s): garbled' % (
+            data[0:4].hex(),
+         )
+         return mess
+      if sub == 0x0c:        # DEVICE ID
+         if dlen == 5 and resp == 'C':
+            mess += ' DEVICE ID 00/0C (%s)' % data[0:4].hex()
+            return mess
+         if dlen == 7 and resp == 'C':
+            mess += ' DEVICE ID 00/0C (%s): channel %d' % (
+               data[0:4].hex(), int.from_bytes(data[5:7])
+            )
+            return mess
+         if dlen == 7 and resp == 'R':
+            mess += ' DEVICE ID 00/0C (%s): channel %d' % (
+               data[0:4].hex(), int.from_bytes(data[5:7])
+            )
+            return mess
+         mess += ' DEVICE ID 00/0C (%s): garbled' % (
+            data[0:4].hex(),
+         )
+         return mess
+      if sub == 0x20:        # TICK (seems to be Rocrail command)
+         if dlen == 8:
+            mess += ' CLOCK 00/20 (%s): %d:%02d tick %ds' % (
+               data[0:4].hex(), int(data[5]), int(data[6]), int(data[7])
+            )
+         return mess
+      if sub == 0x80:        # RESET
+         if dlen != 6:
+            mess += ' RESET 00/80 (%s): garbled' % data[0:4].hex()
+            return mess
+         mess += ' RESET 00/80 (%s): target %d' % (
+            data[0:4].hex(), int(data[5])
+         )
+         return mess
+      if dlen >= 5:       # Unknown, but decodeable
+         mess += ' ***UNKNOWN COMMAND: 00/%02x (%s)' % (
+            sub, data[0:4].hex()
+         )
+         return mess
+      return gen
+         
+   if comm == 0x04:          # LOCO SPEED
+      mess = resp + ' LOCO SPEED 04'
+      if dlen == 4:
+         mess += ' (%s)' % data[0:4].hex()
+         return mess
+      if dlen == 6:
+         mess += ' (%s): %d' % (
+            data[0:4].hex(), int.from_bytes(data[4:6])
+         )
+         return mess
+      mess += ' (garbled)'
+      return mess
+         
+   if comm == 0x05:          # LOCO DIRECTION
+      mess = resp + ' LOCO DIRECTION 05'
+      if dlen == 4:
+         mess += ' (%s)' % data[0:4].hex()
+         return mess
+      if dlen == 5:
+         dirn =  int(data[4])
+         mess += ' (%s): %d - ' % (
+            data[0:4].hex(), dirn
+         )
+         if dirn == 0: mess += 'maintain'
+         elif dirn == 1: mess += 'forward'
+         elif dirn == 2: mess += 'reverse'
+         elif dirn == 3: mess += 'change'
+         else: mess += ' (undefined), maintain'
+         return mess
+      mess += ' (garbled)'
+      return mess
+
+   if comm == 0x06:          # LOCO FUNCTION
+      mess = resp + ' LOCO FUNCTION 06'
+      if dlen == 5:
+         mess += ' (%s): F%d' % (data[0:4].hex(),int(data[4]))
+         return mess
+      if dlen == 6:
+         mess += ' (%s): F%d value %d' % (
+            data[0:4].hex(),
+            int(data[4]),
+            int(data[5])
+         )
+         return mess
+      if dlen == 8:
+         mess += ' (%s): F%d value %d range %d' % (
+            data[0:4].hex(),
+            int(data[4]),
+            int(data[5]),
+            int.from_bytes(data[6:8])
+         )
+         return mess
+      mess += ' (%s): garbled' % data[0:4].hex()
+      return mess
+
+   if comm == 0x0b:          # ACCESSORIES
+      mess = resp + ' ACC 0B '
+      if dlen >= 6:
+         mess += '(%s): posn %d current %d' % (
+            data.hex()[0:8], int(data[4]), int(data[5])
+         )
+      if dlen == 6: return mess
+      if dlen == 8:
+         mess += ' switch time %dx10 ms' % int.from_bytes(data[6:8])
+         return mess
+      mess += '(garbled)'
+      return mess
+
+   if comm == 0x18:          # PING
+      mess = resp + ' PING 18 '
+      if dlen == 0:
+         mess += '(everybody)'
+      else:
+         mess += '(%s): ' % data.hex()[0:8]
+         if data[6:8] == b'\x00\x00':
+            mess += 'Gleis Fmt Processor ver %s' % data[4:6].hex()
+         elif data[6:8] == b'\x00\x10': 
+            mess += 'Gleisbox 601xx ver %s' % data[4:6].hex()
+         elif data[6:8] == b'\x00\x20': 
+            mess += 'Gleisbox 6021 (60128) ver %s' % data[4:6].hex()
+         elif data[6:8] == b'\x00\x30': 
+            mess += 'MS2 60653 etc. ver %s' % data[4:6].hex()
+         elif data[6:8] == b'\x46\xff': 
+            mess += 'Rocrail server ver %s' % data[4:6].hex()
+         elif data[6:8] == b'\xff\xe0': 
+            mess += 'Wireless device ver %s' % data[4:6].hex()
+         elif data[6:8] == b'\xff\xff': 
+            mess += 'CS2-GUI (Master) ver %s' % data[4:6].hex()
+         else:
+            mess += '(unknown) ver %s' % data[4:6].hex()
+      return mess
+
+   if comm == 0x1b:          # CAN BOOT
+      mess = resp + ' CAN BOOT 1B '
+      if dlen == 0:
+         mess += '(everybody)'
+      elif dlen >= 5:
+         mess += '(%s): ' % data.hex()[0:2*dlen]
+      else:
+         mess += '(garbled)'
+      return mess
+
+   if comm == 0x1c:          # RAIL BOOT
+      mess = resp + ' RAIL BOOT 1C '
+      if dlen >= 4:
+         mess += '(%s): ' % data.hex()[0:2*dlen]
+      else:
+         mess += '(garbled)'
+      return mess
+
+   return gen
